998f1266904d51976d8f5798b646e095a888bc78
Jenny Rafferty <raffertyjenny@gmail.com>
2023-03-12 14:33:15 +0000

    tidied code

--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,26 @@
-function sayHello (string) {
-  return `Hello, ${  string  }!`;
-};
+function sayHello(string) {
+  return `Hello, ${string}!`;
+}
 
-function uppercase (string) {
-  return string.toUpperCase()
-};
+function uppercase(string) {
+  return string.toUpperCase();
+}
 
-function lowercase (string) {
+function lowercase(string) {
   return string.toLowerCase();
-};
+}
 
-function countCharacters (string) {
+function countCharacters(string) {
   return string.length;
-};
+}
 
-function firstCharacter (string) {
+function firstCharacter(string) {
   return string.charAt(0);
-};
+}
 
-function firstCharacters (string, n) {
+function firstCharacters(string, n) {
   return string.slice(0, n);
-};
+}
 
 module.exports = {
   sayHello,
